d6973882fcbe7522a9e51f0e71967593d0315e39
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-08-11 17:25:21 -0500

    longest palindrome

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,28 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    let startIndex = 0;
+    let maxLength = 1;
+    
+    function expandAroundMiddle(left, right) {
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentPalLength = right - left + 1;
+            if(currentPalLength > maxLength) {
+                maxLength = currentPalLength;
+                startIndex = left;
+            }
+            left -= 1;
+            right += 1;
+        }
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        expandAroundMiddle(i - 1, i + 1);
+        expandAroundMiddle(i, i + 1);
+    }
+
+    return s.slice(startIndex, startIndex + maxLength);
+}
 
 module.exports = longestPalindrome;
